151a1994dca3e33a855a95989be37f9f314d6128
Jose <matiasjose547@gmail.com>
2018-10-01 11:10:14 -0300

    Criacao do modelo interface 'processosTIG' e suas propriedades

---
+++ src/models/processosTig.ts
@@ -0,0 +1,25 @@
+export interface ProcessosTig {
+
+    titulo: string,
+    subtitulo: string,
+    imagem: string,
+
+    parametros?: {
+        espessuraAproximada: string,
+        posicaoSoldagem? : string,
+        tipoJunta?: string,
+
+        diametroEletrodo: string,
+        diametroadicao: string,
+        velocidadeSoldagem?: string,
+        correnteSoldaPasseRaiz?: string,
+        correnteSoldaOutrosPasses?: string,
+        corrente?: string,
+        tensao?: string,
+        vazaoArgonio?: string,
+        vazaoHelio?: string,
+        numPasses?: string,
+
+
+    }
+}
