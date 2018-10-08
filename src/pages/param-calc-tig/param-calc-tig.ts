f6724633234f07d3c5b8a2fa3c2e6c261da75ee8
Jose <matiasjose547@gmail.com>
2018-10-08 03:44:47 -0300

    iniciando a construção das propriedades e metodos para efetuar a visualização

--- src/pages/param-calc-tig/param-calc-tig.ts
+++ src/pages/param-calc-tig/param-calc-tig.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProcessosTig } from '../../models/processosTig';
+import { OpcoesSelecaoTig } from '../../providers/processo-tig/opcoes-processo-tig';
 
 @IonicPage()
 @Component({
@@ -10,13 +11,31 @@ import { ProcessosTig } from '../../models/processosTig';
 export class ParamCalcTigPage {
 
   public paramProcesso : ProcessosTig;
+  public espessuras;
+  public juntasAcoInoxidavel;
+  public juntasParaAluminio;
+  public posicoesSoldagem;
+
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.paramProcesso = this.navParams.get('processo');
-  }
-
-  ionViewDidLoad() {
+    
+    this.paramProcesso = {
+      id : this.navParams.get('id'),
+      titulo : this.navParams.get('titulo'),
+      subtitulo : this.navParams.get('subtitulo')
+    };
     
   }
+
+  exibirParametrosParaSelecão(){
+    let opcoesSelecaoTig = new OpcoesSelecaoTig;
+    this.espessuras = opcoesSelecaoTig.retornarEspessuras(this.paramProcesso.id);
+  }
+  
+  ionViewDidLoad() {
+    this.exibirParametrosParaSelecão();  
+  }
+
+  
 
 }
