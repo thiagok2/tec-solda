13a2bc71557d806c87896204f7b7935605e5bb16
Jose <matiasjose547@gmail.com>
2018-10-19 12:44:11 -0300

    Definida interface modelo TigProcesso.ts

---
+++ src/models/TigProcesso.ts
@@ -0,0 +1,12 @@
+export interface TigProcesso {
+    id: string,
+    titulo: string,
+    subtitulo: string,
+    imagem?: string,
+
+    espessuraAproximada?: string,
+    posicaoSoldagem?: string,
+    tipoJunta?: string,
+}
+
+
