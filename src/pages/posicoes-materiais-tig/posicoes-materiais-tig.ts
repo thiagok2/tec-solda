f8828e7087debe08ae0d88ad472c35295ff29e8d
Jose <matiasjose547@gmail.com>
2018-10-19 13:31:30 -0300

    Alterações realizadas nas definicoes de interfaces e nomes de variaveis.

--- src/pages/posicoes-materiais-tig/posicoes-materiais-tig.ts
+++ src/pages/posicoes-materiais-tig/posicoes-materiais-tig.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ParamCalcTigPage } from '../param-calc-tig/param-calc-tig';
-import { ProcessosTig } from '../../models/processosTig';
+import { TigProcesso } from '../../models/TigProcesso';
 
 @IonicPage()
 @Component({
@@ -10,36 +10,36 @@ import { ProcessosTig } from '../../models/processosTig';
 })
 export class PosicoesMateriaisTigPage {
 
-  public processosTig: ProcessosTig[];
+  public tigProcesso: TigProcesso[];
   public paramAcoCarbono = {}
 
   constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
   ionViewDidLoad() {
-    this.processosTig = [{
+    this.tigProcesso = [{
       id : 'acoCarbono',
       titulo: 'Aço Carbono',
       subtitulo: 'Soldagem de aço carbono',
       imagem: 'assets/img/posicoes/posicao-plana_reta.png',
-      //parametros: {}
+      
     }, {
       id : 'acoInoxidavel',
       titulo: 'Aço Inoxidável',
       subtitulo: 'Soldagem de aço inoxidável⁽²⁾',
       imagem: 'assets/img/posicoes/posicao-plana_reta.png',
-      //parametros: {}
+      
     }, {
       id : 'aluminioCorrenteAlternada',
       titulo: 'Alumínio : Corrente Alternada⁽¹⁰⁾',
       subtitulo: 'Soldagem de alumínio e sua ligas em corrente alternada⁽¹⁰⁾',
       imagem: 'assets/img/posicoes/posicao-plana_reta.png',
-      //parametros: {}
+      
     }, {
       id : 'aluminioCorrenteContinua',
       titulo: 'Alumínio : Corrente Contínua⁽¹⁰⁾',
       subtitulo: 'Soldagem de alumínio e suas ligas e corrente continua polaridade direta na posição plana ⁽¹⁰⁾',
       imagem: 'assets/img/posicoes/posicao-plana_reta.png',
-      //parametros: {}
+      
     }];
   }
 
