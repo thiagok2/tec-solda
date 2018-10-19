c1cf68cccc57f8f5c0bfe00f153b23f98af23398
Jose <matiasjose547@gmail.com>
2018-10-19 12:48:40 -0300

    Definida interface modelo de resultados 'TigResultado.ts'

---
+++ src/models/TigResultado.ts
@@ -0,0 +1,13 @@
+export interface TigResultado {
+    diametroEletrodo?: string;
+    diametroAdicao?: string;
+    velocidadeSoldagem?: string;
+    correnteSoldaPasseRaiz?: string;
+    correnteSoldaOutrosPasses?: string;
+    corrente?: string;
+    tensao?: string;
+    vazaoArgonio?: string;
+    vazaoHelio?: string;
+    numPasses?: string;
+    tipoJunta?: string;
+}
