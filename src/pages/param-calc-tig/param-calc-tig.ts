b11536edeb2b0e560cef34706700fcb3ad16109a
Jose <matiasjose547@gmail.com>
2018-10-19 13:49:26 -0300

    Adicionado o metodo para calcular os parametros e exibir os resultados do mesmo

--- src/pages/param-calc-tig/param-calc-tig.ts
+++ src/pages/param-calc-tig/param-calc-tig.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ProcessosTig } from '../../models/processosTig';
-import { OpcoesSelecaoTig } from '../../providers/processo-tig/opcoes-processo-tig';
+import { TigProcesso } from '../../models/TigProcesso';
+import { TigProcessoProvider } from '../../providers/tig-processo/tig-processo-provider';
+import { TigOpcoesProvider } from '../../providers/tig-processo/tig-opcoes-provider';
 
 @IonicPage()
 @Component({
@@ -10,32 +11,53 @@ import { OpcoesSelecaoTig } from '../../providers/processo-tig/opcoes-processo-t
 })
 export class ParamCalcTigPage {
 
-  public paramProcesso : ProcessosTig;
+  public paramCalcular: TigProcesso;
+  public calcResultado: TigProcessoProvider;
+  public showResultado: Boolean = false;
   public espessuras;
   public juntasAcoInoxidavel;
   public juntasParaAluminio;
   public posicoesSoldagem;
 
-  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    
-    this.paramProcesso = {
-      id : this.navParams.get('id'),
-      titulo : this.navParams.get('titulo'),
-      subtitulo : this.navParams.get('subtitulo')
+
+    this.paramCalcular = {
+      id: this.navParams.get('id'),
+      titulo: this.navParams.get('titulo'),
+      subtitulo: this.navParams.get('subtitulo'),
+      espessuraAproximada: undefined,
+      posicaoSoldagem: undefined,
+      tipoJunta: undefined
     };
-    
   }
 
-  exibirParametrosParaSelecão(){
-    let opcoesSelecaoTig = new OpcoesSelecaoTig;
-    this.espessuras = opcoesSelecaoTig.retornarEspessuras(this.paramProcesso.id);
+  exibirParametrosParaSelecão() {
+    let opcoesSelecaoTig = new TigOpcoesProvider();
+    if (this.paramCalcular.id == 'acoInoxidavel') {
+      this.juntasAcoInoxidavel = opcoesSelecaoTig.TIPO_JUNTA_ACO_INOXIDAVEL;
+    }
+    if (this.paramCalcular.id == 'aluminioCorrenteAlternada') {
+      this.posicoesSoldagem = opcoesSelecaoTig.POSICAO_SOLDAGEM;
+    }
+    if (this.paramCalcular.id == 'aluminioCorrenteContinua') {
+      this.juntasParaAluminio = opcoesSelecaoTig.TIPO_JUNTA_ALUMINIO_CORRENTE_CONTINUA;
+    }
+    this.espessuras = opcoesSelecaoTig.retornarEspessuras(this.paramCalcular.id);
   }
-  
+
   ionViewDidLoad() {
-    this.exibirParametrosParaSelecão();  
+    this.exibirParametrosParaSelecão();
   }
 
-  
+  eventSelectEspessura(event: any) {
+    this.calcular();
+  }
+
+
+  calcular() {
+    this.showResultado = true;
+    this.calcResultado = new TigProcessoProvider(this.paramCalcular);
+  }
+
 
 }
