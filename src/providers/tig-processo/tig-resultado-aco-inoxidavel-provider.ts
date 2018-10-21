7be93f930f31dbc971a5071ac964e8a73c7ff3dc
Jose <matiasjose547@gmail.com>
2018-10-20 23:52:11 -0300

    Criada classe 'TigResultadoAcoInoxidavelProvider' criada para conter os resultados para os calculos com aço inoxidavel

---
+++ src/providers/tig-processo/tig-resultado-aco-inoxidavel-provider.ts
@@ -0,0 +1,96 @@
+import { TigResultado } from "../../models/TigResultado";
+
+export class TigResultadoAcoInoxidavelProvider {
+
+    private _acoInoxidavelValores = new Map();
+    private _tigResutados: TigResultado;
+
+    constructor() {
+        this.defineObjetoAcoInoxidavelValores();
+    }
+
+    getValorAcoInoxidavel(especura, tipoJunta) {
+        let chave = especura + '&' + tipoJunta;
+        console.log(chave);
+
+        this._tigResutados = this._acoInoxidavelValores.get(chave);
+        return this._tigResutados;
+    }
+
+    defineObjetoAcoInoxidavelValores() {
+
+        this._acoInoxidavelValores.set('0.6&Sem Chanfro Sem Abertura', {
+            tipoJunta: '1',
+            diametroAdicao: '15 - 25',
+            corrente: '3',
+            vazaoArgonio: '1',
+            numPasses: '30 - 40'
+        });
+        this._acoInoxidavelValores.set('0.8&Sem Chanfro Sem Abertura', {
+            tipoJunta: '1',
+            diametroAdicao: '15 - 30',
+            corrente: '3',
+            vazaoArgonio: '1',
+            numPasses: '30 - 40'
+        });
+        this._acoInoxidavelValores.set('1.0&Sem Chanfro Com Abertura', {
+            tipoJunta: '1',
+            diametroEletrodo: '1',
+            diametroAdicao: '25 - 60',
+            corrente: '4',
+            vazaoArgonio: '1',
+            numPasses: '25 - 30'
+        });
+        this._acoInoxidavelValores.set('1.5&Sem Chanfro Com Abertura', {
+            tipoJunta: '1.5',
+            diametroEletrodo: '1.5',
+            diametroAdicao: '50 - 80',
+            corrente: '4',
+            vazaoArgonio: '1',
+            numPasses: '25 - 30'
+        });
+        this._acoInoxidavelValores.set('2.0&Sem Chanfro Com Abertura', {
+            tipoJunta: '1.5',
+            diametroEletrodo: '1.5 - 2.0',
+            diametroAdicao: '80 - 110',
+            corrente: '4',
+            vazaoArgonio: '1',
+            numPasses: '25 - 30'
+        });
+        this._acoInoxidavelValores.set('3.0&Sem Chanfro Com Abertura', {
+            tipoJunta: '2',
+            diametroEletrodo: '2 - 3',
+            diametroAdicao: '100 - 150',
+            corrente: '4',
+            vazaoArgonio: '1',
+            numPasses: '25 - 30'
+        });
+        this._acoInoxidavelValores.set('4.0&Chanfro em V', {
+            tipoJunta: '2',
+            diametroEletrodo: '3',
+            diametroAdicao: '120 - 200',
+            corrente: '5',
+            vazaoArgonio: '1',
+            numPasses: '25'
+        });
+        this._acoInoxidavelValores.set('5.0&Chanfro em V', {
+            tipoJunta: '3',
+            diametroEletrodo: '3 - 4',
+            diametroAdicao: '200 - 250',
+            corrente: '5',
+            vazaoArgonio: '1',
+            numPasses: '25'
+        });
+        this._acoInoxidavelValores.set('6.0&Chanfro em V', {
+            tipoJunta: '3',
+            diametroEletrodo: '4',
+            diametroAdicao: '200 - 250',
+            corrente: '6',
+            vazaoArgonio: '2',
+            numPasses: '25'
+        });
+
+    }
+
+
+}
