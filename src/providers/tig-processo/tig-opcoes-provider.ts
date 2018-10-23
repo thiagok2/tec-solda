95f20080a5c39312f12247915e7cc8de5449aecd
Jose <matiasjose547@gmail.com>
2018-10-23 15:06:00 -0300

    Retocando exibição das espeçuras e tipo de junta para aluminio para melhor manipulação dos valores.

--- src/providers/tig-processo/tig-opcoes-provider.ts
+++ src/providers/tig-processo/tig-opcoes-provider.ts
@@ -3,11 +3,11 @@ export class TigOpcoesProvider {
     static EXPESSURA_ACO_CARBONO = ['0.5', '0.8', '1.0', '1.2', '1.5', '2.0'];
     static EXPESSURA_ACO_INOXIDAVEL = ['0.6', '0.8', '1.0', '1.5', '2.0', '3.0', '4.0', '5.0', '6.0'];
     static ESPESSURA_ALUMINIO_CORRENTE_ALTERNADA = ['1.6', '3.2', '6.4', '9.5', '12.7', '25.4'];
-    static ESPESSURA_ALUMINIO_CORRENTE_CONTINUA = ['0.50', '0.75', '0.80', '1.00', '1.25', '1.50', '1.75', '2.00', '2.25', '3.20', '6.30', '12.70', '19.0', '25.4'];
+    static ESPESSURA_ALUMINIO_CORRENTE_CONTINUA = ['0.5', '0.75', '0.8', '1.0', '1.25', '1.5', '1.75', '2.0', '2.25', '3.2', '6.3', '12.7', '19.0', '25.4'];
 
     static TIPO_JUNTA_ACO_INOXIDAVEL = ['Sem Chanfro Sem Abertura', 'Sem Chanfro Com Abertura', 'Chanfro em V'];
     static POSICAO_SOLDAGEM = ['PLANA', 'HORIZONTAL,VERTICAL', 'SOBRE-CABEÇA'];
-    static TIPO_JUNTA_ALUMINIO_CORRENTE_CONTINUA = ['Solda sem chanfro', 'V-60° 3,0 mm solda sem chanfro', 'V-60° ou duplo V; raiz 5,0 mm solda sem chanfro '];
+    static TIPO_JUNTA_ALUMINIO_CORRENTE_CONTINUA = ['Solda sem chanfro', 'V-60° 3,0 mm solda sem chanfro', 'V-60° ou duplo V; raiz 5,0 mm solda sem chanfro'];
 
 
     retornarEspessuras(tipoEspessuras) {
