e59aed1f742666424ea497326f0d7c176b74fb3d
jose <matiasjose547@gmail.com>
2019-08-01 03:55:55 -0300

    adicionada as funcionalidades de login com facebook e google, no menu lateral do app. No arquivo app.componet.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, AlertController, ToastController, LoadingController } from 'ionic-angular';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Facebook } from '@ionic-native/facebook';
+import firebase from 'firebase/app';
 
 import { FirstRunPage } from '../pages/pages';
 
@@ -10,7 +15,16 @@ import { FirstRunPage } from '../pages/pages';
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   rootPage = FirstRunPage;
+
+  private statusOfConnection = undefined;
+  private user = {
+    id: undefined,
+    displayName: undefined,
+    email: undefined,
+    imageUrl: undefined
+  }
 
   @ViewChild(Nav) nav: Nav;
 
@@ -25,7 +39,17 @@ export class MyApp {
     { title: ' Sobre', component: 'AboutPage', icon: 'contacts' }
   ];
 
-  constructor(private translate: TranslateService, platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  constructor(private translate: TranslateService,
+    public platform: Platform,
+    private statusBar: StatusBar,
+    private splashScreen: SplashScreen,
+    private googlePlus: GooglePlus,
+    private facebook: Facebook,
+    private afAuth: AngularFireAuth,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -40,7 +64,7 @@ export class MyApp {
     this.translate.setDefaultLang('pt-br');
     //const browserLang = this.translate.getBrowserLang();
 
-  
+
   }
 
   openPage(page) {
@@ -48,4 +72,92 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  connected() {
+    return this.afAuth.authState.subscribe((user: firebase.User) => {
+      if (!user) {
+        this.user = null;
+        return;
+      }
+      this.user.displayName = user.displayName;
+      return true;
+    });
+  }
+
+  loginWithGoogle() {
+    const load = this.loading();
+    load.present();
+    this.googlePlus.login({})
+      .then(res => {
+        this.user.id = res.userId;
+        this.user.email = res.email;
+        this.user.displayName = res.displayName;
+        this.user.imageUrl = res.imageUrl;
+        this.statusOfConnection = 'connected';
+        this.toast('Você esta conectado.')
+      })
+      .catch(err => {
+        console.log(err);
+        this.alert();
+      });
+    load.dismiss();
+  }
+
+  loginWithFacebook() {
+    const load = this.loading();
+    load.present();
+    this.facebook.login(['email'])
+      .then(response => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider
+          .credential(response.authResponse.accessToken);
+
+        firebase.auth().signInWithCredential(facebookCredential)
+          .then(success => {
+            this.user.id = response.authResponse.userID;
+            this.user.displayName = success.displayName;
+            this.user.email = success.email;
+            this.user.imageUrl = `https://graph.facebook.com/${response.authResponse.userID}/picture?type=large`;
+            this.statusOfConnection = 'connected';
+            this.toast('Você esta conectado.')
+          })
+          .catch(err => this.alert());
+
+      }).catch((err) => {
+        console.log(err);
+        this.alert();
+      });
+    load.dismiss();
+  }
+
+  logout() {
+    this.statusOfConnection = false;
+    this.afAuth.auth.signOut();
+    this.googlePlus.logout();
+    this.user = {
+      id: undefined,
+      displayName: undefined,
+      email: undefined,
+      imageUrl: undefined
+    };
+  }
+
+  loading() {
+    return this.loadingCtrl.create({ content: 'aguarde..' });
+  }
+
+  toast(message) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    })
+  }
+
+  alert() {
+    this.alertCtrl.create({
+      message: 'Não foi possivel realizar o login com Google. Verifique sua conexão com a internet..',
+      buttons: ['Ok']
+    }).present();
+  }
+
+
 }
