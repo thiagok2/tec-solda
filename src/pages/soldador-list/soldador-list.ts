2d3466deb3c126fd12f3a30cf83bd03b3339a988
jose <matiasjose547@gmail.com>
2019-08-01 04:24:00 -0300

    removido metodos de login com facebook e google, de soldadorlist page

--- src/pages/soldador-list/soldador-list.ts
+++ src/pages/soldador-list/soldador-list.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 import { SoldadorElement } from '../../models/soldador';
 import { SoldadorProvider } from '../../providers/soldador/soldador';
-
-import { AngularFireAuth } from '@angular/fire/auth';
-import firebase from 'firebase/app';
-import { GooglePlus } from '@ionic-native/google-plus';
-import { Facebook } from '@ionic-native/facebook';
 
 @IonicPage()
 @Component({
@@ -15,27 +10,14 @@ import { Facebook } from '@ionic-native/facebook';
 	templateUrl: 'soldador-list.html',
 })
 export class SoldadorListPage {
-	private displayName;
 	soldadorList: SoldadorElement[] = [];
-	private user: any = {};
 
 	constructor(
-		public alertCtrl: AlertController,
-		private googlePlus: GooglePlus,
-		private facebook: Facebook,
-		private afAuth: AngularFireAuth,
 		public navCtrl: NavController,
 		public navParams: NavParams,
 		private soldadorProvider: SoldadorProvider,
 		private toast: ToastController) {
-
-		afAuth.authState.subscribe((user: firebase.User) => {
-			if (!user) {
-				this.displayName = null;
-				return;
-			}
-			this.displayName = user.displayName;
-		});
+	
 	}
 
 	ionViewDidLoad() {
@@ -53,49 +35,6 @@ export class SoldadorListPage {
 			{ key: item.key, soldador: item.soldador });
 	}
 
-	loginGoogle() {
-
-		this.googlePlus.login({})
-			.then(res => {
-				this.user.id = res.userId;
-				this.user.email = res.email;
-				this.user.name = res.displayName;
-				this.user.image = res.imageUrl;
-			})
-			.catch(err => {
-				this.alertPresent();
-			});
-	}
-
-	loginFacebook(){
-		this.facebook.login(['email'])
-			.then(response => {
-				const facebookCredential = firebase.auth.FacebookAuthProvider
-					.credential(response.authResponse.accessToken);
-
-				firebase.auth().signInWithCredential(facebookCredential)
-					.then(success => {
-						this.user.id = '';
-						this.user.name = '';
-						this.user.email = '';
-						this.user.image = '';
-
-						console.log(this.user);
-						console.log("Firebase success: " + JSON.stringify(success));
-					});
-
-			}).catch((err) => {
-				this.alertPresent();
-				console.log(err);
-			});
-	}
-
-	logout() {
-		this.afAuth.auth.signOut();
-		this.googlePlus.logout();
-		this.user = {};
-	}
-
 	removerSoldador(item: SoldadorElement) {
 		this.soldadorProvider.remove(item.key)
 			.then(() => {
@@ -105,14 +44,5 @@ export class SoldadorListPage {
 				this.toast.create({ message: 'Soldador removido.', duration: 3000, position: 'botton' }).present();
 			})
 	}
-
-	alertPresent() {
-		this.alertCtrl.create({
-			title: 'Aviso',
-			message: 'Não foi possivel o efetuar o login. Por favor, verifique sua conexão e tente novamente..',
-			buttons: ['Ok']
-		}).present();
-	}
-
 
 }
