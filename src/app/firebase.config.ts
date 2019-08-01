7f0caa3b8edce97e3f7ae038f3c99ee5e7ad6a9c
jose <matiasjose547@gmail.com>
2019-08-01 04:07:27 -0300

    adicionado arquivo de credenciais do firebase

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDnw24rW6yikQ3MJYNcTaXq_0mWnjWQVYA",
-    authDomain: "ionic-login-235214.firebaseapp.com",
-    databaseURL: "https://ionic-login-235214.firebaseio.com",
-    projectId: "ionic-login-235214",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "24478018113",
-    appId: "1:24478018113:web:9176e352af35735d"
+    messagingSenderId: "",
+    appId: ""
 };
