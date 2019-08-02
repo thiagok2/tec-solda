5552e63c3cb5179bd56cb7246fc00aedeff2f1ca
jose <matiasjose547@gmail.com>
2019-08-02 00:21:12 -0300

    consertado bug de selecao de imagens de inspecao em new-inspecao page

--- src/pages/inspecao-new/inspecao-new.ts
+++ src/pages/inspecao-new/inspecao-new.ts
@@ -9,7 +9,7 @@ import { FilePath } from '@ionic-native/file-path';
 import { Inspecao } from '../../models/inspecao'
 
 declare var cordova: any;
- 
+
 
 @IonicPage()
 @Component({
@@ -18,134 +18,130 @@ declare var cordova: any;
 })
 export class InspecaoNewPage {
 
-	  /**
-     * Declarando a variável onde será adicionado o base64 da foto
-     */
-	  currentPhoto;
+  /**
+   * Declarando a variável onde será adicionado o base64 da foto
+   */
+  currentPhoto;
 
 
-    lastImage: string = null;
+  lastImage: string = null;
 
-  	constructor(
-  			public navCtrl: NavController, 
-  			public navParams: NavParams,
-  			public camera : Camera,
-        private file: File, private filePath: FilePath,
-        public toastCtrl: ToastController, public platform: Platform) {
-  	}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public camera: Camera,
+    private file: File, private filePath: FilePath,
+    public toastCtrl: ToastController, public platform: Platform) {
+  }
 
-  	
-  	getPhoto(type){
 
-   		const options: CameraOptions = {
-    		quality: 50,
-      		destinationType: this.camera.DestinationType.DATA_URL,
-      		encodingType: this.camera.EncodingType.JPEG,
-      		mediaType:this.camera.MediaType.PICTURE,
-      		sourceType: type == "picture" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.SAVEDPHOTOALBUM,
-      		correctOrientation: true
-    	};
+  getPhoto(type) {
 
-    	this.camera.getPicture(options).then((imageData) => {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: type == "picture" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      correctOrientation: true
+    };
 
-    	  this.currentPhoto = 'data:image/jpeg;base64,' + imageData;
+    this.camera.getPicture(options).then((imageData) => {
 
-    	}, (err) => {
+      this.currentPhoto = 'data:image/jpeg;base64,' + imageData;
+
+    }, (err) => {
       // Handle error
-    	});
- 	  }
+    });
+  }
 
-    takePhoto(type){
-      let sourceType;
-      if(this.platform.is(cordova)){
-        sourceType = type == "picture" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.PHOTOLIBRARY
-      }else{
-        
-      }
-      let options = {
-          quality: 50,
-          sourceType: sourceType,
-          saveToPhotoAlbum: true,
-          correctOrientation: true
-      };
+  takePhoto(type) {
+    let sourceType = type == "picture" ? this.camera.PictureSourceType.CAMERA : this.camera.PictureSourceType.PHOTOLIBRARY;
 
-      this.camera.getPicture(options).then((imagePath) => {
-        if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
-          this.filePath.resolveNativePath(imagePath)
-            .then(filePath => {
-              let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-              let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
-              this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-            });
-        }else {
-          let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-          let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-        }
+    let options = {
+      quality: 50,
+      sourceType: sourceType,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    };
 
-      }, (err) => {
-        this.presentToast('Problema ao selecionar imagem.');
-      });
-    }
-
-    private presentToast(text) {
-      let toast = this.toastCtrl.create({
-        message: text,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    }
-
-    // Create a new name for the image
-    private createFileName() {
-      let d = new Date(),
-      n = d.getTime(),
-      newFileName =  'inspecao_'+n + ".jpg";
-      return newFileName;
-    }
-     
-    // Copy the image to a local folder
-    private copyFileToLocalDir(namePath, currentName, newFileName) {
-      this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-        this.lastImage = newFileName;
-        this.presentToast('Imagem salva com sucesso.');
-      }, error => {
-        this.presentToast('Erro ao gravar imagem.');
-      });
-    }
-
-    public pathForImage(img) {
-      if (img === null) {
-        return '';
+    this.camera.getPicture(options).then((imagePath) => {
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
       } else {
-        return cordova.file.dataDirectory + img;
+        let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
       }
+
+    }, (err) => {
+      this.presentToast('Problema ao selecionar imagem.');
+    });
+  }
+
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+  // Create a new name for the image
+  private createFileName() {
+    let d = new Date(),
+      n = d.getTime(),
+      newFileName = 'inspecao_' + n + ".jpg";
+    return newFileName;
+  }
+
+  // Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+      this.presentToast('Imagem salva com sucesso.');
+    }, error => {
+      this.presentToast('Erro ao gravar imagem.');
+    });
+  }
+
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
     }
+  }
 
- 	  avancar(){
+  avancar() {
 
-   	 	let novaInspecao = new Inspecao({});
+    let novaInspecao = new Inspecao({});
 
-   	 	//novaInspecao.foto = this.currentPhoto;
-      novaInspecao.foto = this.currentPhoto != null ? this.currentPhoto : this.lastImage;
-
-
-      let toast = this.toastCtrl.create({
-        message: novaInspecao.foto,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
+    //novaInspecao.foto = this.currentPhoto;
+    novaInspecao.foto = this.currentPhoto != null ? this.currentPhoto : this.lastImage;
 
 
-   	 	this.navCtrl.push('InspecaoEditPage',{
-   	 		inspecao: novaInspecao
-   	 	})
-   	}
+    let toast = this.toastCtrl.create({
+      message: novaInspecao.foto,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
 
-    voltar(){
-      this.navCtrl.setRoot('InspecaoListPage');
-    }
+
+    this.navCtrl.push('InspecaoEditPage', {
+      inspecao: novaInspecao
+    })
+  }
+
+  voltar() {
+    this.navCtrl.setRoot('InspecaoListPage');
+  }
 
 }
