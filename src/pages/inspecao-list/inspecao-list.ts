be0154fa403a2e9346d9d0033288ab0fca17ed91
jose <matiasjose547@gmail.com>
2019-08-10 10:31:25 -0300

    adicionado funcioanlidades de criacao de pdf, deletar inspeção.

--- src/pages/inspecao-list/inspecao-list.ts
+++ src/pages/inspecao-list/inspecao-list.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ToastController, AlertController, LoadingController } from 'ionic-angular';
 
-import { InspecaoElement } from '../../models/inspecao';
+import { InspecaoElement, Inspecao } from '../../models/inspecao';
 
 import { InspecaoProvider } from '../../providers/providers';
+import { StorageFireProvider } from '../../providers/database/storagefire';
+import { PdfCreateProvider } from '../../providers/create-pdf/pdf-create';
+import { FileOpener } from '@ionic-native/file-opener';
+import { File } from '@ionic-native/file';
+
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 declare var cordova: any;
 
@@ -19,9 +28,19 @@ export class InspecaoListPage {
 	constructor(public navCtrl: NavController,
 		public navParams: NavParams,
 		public items: InspecaoProvider,
-		public modalCtrl: ModalController) { }
+		public storageDB: StorageFireProvider,
+		private fileOpener: FileOpener,
+		private file: File,
+		public modalCtrl: ModalController,
+		public toastCtrl: ToastController,
+		public alertCtrl: AlertController,
+		public loadingCtrl: LoadingController) { }
 
 	ionViewDidLoad() {
+		this.upgradeInspections();
+	}
+
+	upgradeInspections() {
 		this.items.getAll().then((result) => {
 			this.currentItems = result;
 		});
@@ -38,10 +57,63 @@ export class InspecaoListPage {
 	}
 
 	novaInspecao() {
-		this.navCtrl.setRoot('InspecaoNewPage');
+		this.navCtrl.push('InspecaoNewPage');
+	}
+	
+	deleteCardInspection(key) {
+		this.items.delete(key);
+		this.toast('Inspeção apagada com sucesso!');
+		this.upgradeInspections();
+	}
+	
+	async uploadCardInspection(i: Inspecao){
+		this.toast('Serviço indisponivel no momento..');
+		return;
+		const filePath = this.pathForImage(i.foto);
+		this.storageDB.pushUpload(filePath);
+		//console.log(content)
 	}
 
-	retornaIcones(icon: boolean) {
-		return icon ? "checkmark" : "close";
+	toast(message) {
+		const toast = this.toastCtrl.create({
+			message: message,
+			duration: 3000
+		});
+		toast.present();
 	}
+
+	createPdf(inspection) {
+		const loading = this.loadingCtrl.create({content: 'Aguarde ..'});
+		loading.present();
+		let YOUR_DEFINITION_HERE = new PdfCreateProvider(inspection).returnPdf();
+		
+		pdfMake.createPdf(YOUR_DEFINITION_HERE).getBlob(buffer => {
+			this.file.resolveDirectoryUrl(this.file.externalRootDirectory)
+				.then(dirEntry => {
+					this.file.getFile(dirEntry, `${inspection.key}.pdf`, { create: true })
+						.then(fileEntry => {
+							fileEntry.createWriter(writer => {
+								writer.onwrite = () => {
+									this.fileOpener.open(fileEntry.toURL(), 'application/pdf')
+										.then(res => { loading.dismiss()})
+										.catch(err => {
+											const alert = this.alertCtrl.create({ message: err.message, buttons: ['Ok'] });
+											alert.present();
+										});
+								}
+								writer.write(buffer);
+							})
+						})
+						.catch(err => {
+							const alert = this.alertCtrl.create({ message: err, buttons: ['Ok'] });
+							alert.present();
+						});
+				})
+				.catch(err => {
+					const alert = this.alertCtrl.create({ message: err, buttons: ['Ok'] });
+					alert.present();
+				});
+		});
+	}
+
 }
